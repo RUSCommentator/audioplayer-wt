3ba255eea73276d07979ce366d30ac2ee8c14067
RUSCommentator <129686980+RUSCommentator@users.noreply.github.com>
2023-08-07 19:34:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import pytube as pt
+import vlc
+
+def zxc(url):
+    player = vlc.MediaPlayer(url)
+    player.play()
+    input("Playing audio. Press any key to stop.")
+    player.stop()
+
+def audioxoxol(url):
+    yt = pt.YouTube(url)
+    audio = yt.streams.filter(only_audio=True).first()
+    audio.download()
+    audio_file_path = audio.default_filename
+    play_audio(audio_file_path)
+
+if __name__ == '__main__':
+    video_url = input("Enter URL: ")
+    download_and_play_audio(video_url)
